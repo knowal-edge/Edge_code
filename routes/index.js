3debe54d63be1dcfd0a7f5296831f85ce7ded470
ananth4u <ananth4u@ananth4us-MacBook-Pro.local>
2019-07-06 00:10:03 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,17 @@ var express = require('express');
 var router = express.Router();
 var mqttService = require('../src/services/mqttServices');
 var websocket = require('../src/services/websocketService');
-var encryption = require('../src/services/encryptionService');
-var mailService = require('../src/services/mailService');
 
-mqttService.mqttPublisher("30");
-//websocket.socketServer();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
- // mqttService.mqttSubscriber();
-
+websocket.socketServer();
+mqttService.mqttPublisher();
+/* GET Webscoket page. */
+router.get('/websocket', function(req, res, next) {
+ 
 });
 
- 
- 
+/* GET MQTT page. */
+router.get('/mqtt', function(req, res, next) {
+  
+});
 
 module.exports = router;
--- src/services/mqttServices.js
+++ src/services/mqttServices.js
@@ -1,6 +1,7 @@
 var mqtt = require('mqtt');
 var url = require('url');
 require('dotenv').config()
+const axios = require("axios");
 
 var options={
     retain:true,
@@ -8,58 +9,24 @@ var options={
 
 var client  = mqtt.connect(process.env.MQTTURL);
 var edTopic=process.env.EDGETOPIC;
-var seTopic=process.env.SERVERTOPIC;
 
 
 module.exports = {
      /*******************************MQTT PUBLISHER METHOD********************************************/
 
-     mqttPublisher:function(msg1){
+     mqttPublisher:function(){
 
         client.on("connect",function(){	
-            console.log("connected  "+ client.connected);
+            console.log("MQTT connected  "+ client.connected);
         })
 
         //handle errors
         client.on("error",function(error){
-            console.log("Can't connect" + error);
+            console.log("MQTT Can't connect" + error);
             process.exit(1)
         });
-
-
-        if (client.connected == true){
-	
-            client.publish(edTopic,msg1,options);
-            
-            }
-            
-    },
-
-    /*******************************END********************************** */
-
-    /*******************************MQTT SUBSCRIPTION METHOD********************************************/
-
-    mqttSubscriber:function(){
-
-        client.on("connect",function(){	
-            console.log("connected  "+ client.connected);
-        })
-
-
-        //handle incoming messages
-        client.on('message',function(edTopic, message, packet){
-	        console.log("MQTT Message Queue from Edge to Server is "+ message);
-	        console.log("Edge Topic is "+ edTopic);
-        });
-
-        //handle errors
-        client.on("error",function(error){
-            console.log("Can't connect" + error);
-            process.exit(1)
-        });
-
-        client.subscribe(edTopic,{qos:1}); //single topic
-    }
+             client.publish(edTopic,"50",options); //Publishing sensor data to server
+            },
 
     /*******************************END********************************** */
 
